b9b8eb8c76f0e69d1b7143ada834d543576f2d1c
HafsaTazrian <hafsa.tazrian@gmail.com>
2024-05-27 07:06:22 +0600

    Javadoc by Hafsa

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.travel_app.Domains","l":"CategoryDomain"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.travel_app","l":"MainActivity2Test"}];updateSearchResults();
